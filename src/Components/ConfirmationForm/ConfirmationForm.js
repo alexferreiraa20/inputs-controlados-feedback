6fed6e3fc743c0a103815501a576e00673a1535a
Alex <alexferreiraa20@gmail.com>
2022-10-18 16:19:56 -0300

    quase feito

--- src/Components/ConfirmationForm/ConfirmationForm.js
+++ src/Components/ConfirmationForm/ConfirmationForm.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form} from '../MainPage/styles'
+// import {useState} from 'react'
+
+
+
 
 const ConfirmationForm = () => {
+
+  const [data, setData] = useState("")
+
+  const onChangeData = (event) => {
+    console.log(event.target.value)
+    setData(event.target.value)
+  } 
+
   return (
     <Form>
+        <h2>Form exercicio 3</h2>
+        <label>
+        Data de nascimento: 
+        <input type = "date" onChange={onChangeData} value= {data}/>
+        </label>
+        <label>
+          Gênero: 
+          <select >
+            <option value = "1">Homem cisgênero</option>
+            <option value = "1">Mulher cisgênero</option>
+            <option value = "1">Homem trans</option>
+            <option value = "1">Mulher trans</option>
+            <option value = "1">Pessoa não binária</option>
+            <option value = "1">Prefiro não dizer</option>
+          </select>
+
+        </label>
+
+
+
+
         
       <button>Enviar dados</button>
       </Form>
